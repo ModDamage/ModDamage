42320d17f7d9c4ea1a7c3e5e2875e021d3f6c1a0
Erich Gubler <k0ryu0bihir0@gmail.com>
2011-06-20 21:35:04 -0600

    Deleted some things. :x

--- src/com/KoryuObihiro/bukkit/ModDamage/CalculationObjects/Damage/DamageCalculation.java
+++
@@ -1,3 +0,0 @@
-package com.KoryuObihiro.bukkit.ModDamage.CalculationObjects.Damage;
-
-public abstract class DamageCalculation{ public abstract int calculate(int eventDamage);}
--- src/com/KoryuObihiro/bukkit/ModDamage/CalculationObjects/Damage/EventDamageRollCalculation.java
+++
@@ -1,7 +0,0 @@
-package com.KoryuObihiro.bukkit.ModDamage.CalculationObjects.Damage;
-
-public class EventDamageRollCalculation extends ChanceCalculation 
-{
-	@Override
-	public int calculate(int eventDamage){ return Math.abs(random.nextInt()%(eventDamage + 1));}
-}
