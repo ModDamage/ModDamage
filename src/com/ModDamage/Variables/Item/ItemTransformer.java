1f77d445a7e09de77d7ba55eabc7c64461d49765
Matt Peterson <ricochet1k@gmail.com>
2012-08-14 11:41:51 -0600

    Item -> Material transformer (hacky?)

--- src/com/ModDamage/Variables/Item/ItemTransformer.java
+++ src/com/ModDamage/Variables/Item/ItemTransformer.java
@@ -2,12 +2,13 @@ package com.ModDamage.Variables.Item;
 
 import java.util.regex.Matcher;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Item;
 import org.bukkit.inventory.ItemStack;
 
 import com.ModDamage.StringMatcher;
-import com.ModDamage.Backend.BailException;
 import com.ModDamage.EventInfo.DataProvider;
+import com.ModDamage.EventInfo.DataProvider.IDataParser;
 import com.ModDamage.EventInfo.EventData;
 import com.ModDamage.EventInfo.EventInfo;
 import com.ModDamage.EventInfo.IDataProvider;
@@ -17,27 +18,27 @@ public class ItemTransformer
 
 	public static void register()
 	{
-		DataProvider.registerTransformer(ItemStack.class, Item.class, new DataProvider.IDataParser<ItemStack, Item>()
-			{
-				@Override
-				public IDataProvider<ItemStack> parse(EventInfo info, Class<?> want, IDataProvider<Item> itemDP, Matcher m, StringMatcher sm)
-				{
-					return new DataProvider<ItemStack, Item>(Item.class, itemDP)
-						{
-							@Override
-							public ItemStack get(Item start, EventData data) throws BailException
-							{
-								return start.getItemStack();
-							}
-
-							@Override
-							public Class<ItemStack> provides()
-							{
-								return ItemStack.class;
-							}
-						};
-				}
+		DataProvider.registerTransformer(ItemStack.class, Item.class,
+				new DataProvider.IDataParser<ItemStack, Item>() {
+					public IDataProvider<ItemStack> parse(EventInfo info, Class<?> want, IDataProvider<Item> itemDP, Matcher m, StringMatcher sm) {
+						return new DataProvider<ItemStack, Item>(Item.class, itemDP) {
+								public ItemStack get(Item start, EventData data) { return start.getItemStack(); }
+								public Class<ItemStack> provides() { return ItemStack.class; }
+								public String toString() { return startDP.toString(); }
+							};
+					}
 			});
+		
+		DataProvider.registerTransformer(Material.class, Item.class,
+				new IDataParser<Material, Item>() {
+					public IDataProvider<Material> parse(EventInfo info, Class<?> want, IDataProvider<Item> blockDP, Matcher m, StringMatcher sm) {
+						return new DataProvider<Material, Item>(Item.class, blockDP) {
+								public Material get(Item block, EventData data) { return block.getItemStack().getType(); }
+								public Class<Material> provides() { return Material.class; }
+								public String toString() { return startDP.toString(); }
+							};
+					}
+				});
 	}
 
 }
