43954172b312544935cfaf731f17b2d0133c8bf2
Matt Peterson <ricochet1k@gmail.com>
2012-10-09 20:46:21 -0600

    ProjectileHit for arrows now has hitblock for the block that it hit. Use !hitblock.is.none to test if there is one.

--- src/com/ModDamage/Events/Entity/ProjectileHit.java
+++ src/com/ModDamage/Events/Entity/ProjectileHit.java
@@ -1,6 +1,14 @@
 package com.ModDamage.Events.Entity;
 
+import java.lang.reflect.Field;
+
+import net.minecraft.server.EntityArrow;
+
+import org.bukkit.Bukkit;
 import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.craftbukkit.entity.CraftArrow;
+import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Projectile;
@@ -19,8 +27,33 @@ public class ProjectileHit extends MDEvent implements Listener
 {
 	public ProjectileHit() { super(myInfo); }
 	
+	// Try the magic way to get hit block from EntityArrow
+	private static Field inGroundProp, xprop, yprop, zprop;
+	private static boolean tryMagic = false;
+	static {
+		try
+		{
+			inGroundProp = EntityArrow.class.getDeclaredField("inGround");
+			xprop = EntityArrow.class.getDeclaredField("d");
+			yprop = EntityArrow.class.getDeclaredField("e");
+			zprop = EntityArrow.class.getDeclaredField("f");
+
+			inGroundProp.setAccessible(true);
+			xprop.setAccessible(true);
+			yprop.setAccessible(true);
+			zprop.setAccessible(true);
+			
+			System.out.println("Trying ProjectileHit magic for arrow hit block");
+			
+			tryMagic = true;
+		}
+		catch (SecurityException e) { e.printStackTrace(); }
+		catch (NoSuchFieldException e) { e.printStackTrace(); }
+	}
+	
 	static final EventInfo myInfo = new SimpleEventInfo(
 			Entity.class, 		"shooter", "entity",
+			Block.class, 		"hitblock",
 			Projectile.class,	"projectile",
 			World.class,		"world");
 	
@@ -29,14 +62,72 @@ public class ProjectileHit extends MDEvent implements Listener
 	{
 		if(!ModDamage.isEnabled) return;
 		
-		Projectile projectile = (Projectile)event.getEntity();
-		LivingEntity shooter = projectile.getShooter();
+		final Projectile projectile = event.getEntity();
+		final LivingEntity shooter = projectile.getShooter();
 		
+		if (tryMagic && projectile instanceof Arrow) {
+			magicWay(shooter, (Arrow) projectile);
+		}
+		else
+		{
+			normalWay(shooter, projectile);
+		}
+		
+	}
+	
+
+	private void normalWay(LivingEntity shooter, Projectile projectile)
+	{
 		EventData data = myInfo.makeData(
 				shooter,
+				null,
 				projectile,
 				projectile.getWorld());
 		
 		runRoutines(data);
+	}
+	
+	private void magicWay(final LivingEntity shooter, final Arrow arrow)
+	{
+		Bukkit.getScheduler().scheduleSyncDelayedTask(ModDamage.getPluginConfiguration().plugin, new Runnable() {
+			public void run()
+			{
+				if (arrow.isDead()) return;
+				
+				EntityArrow earrow = ((CraftArrow) arrow).getHandle();
+				
+				
+				boolean inGround;
+				int x, y, z;
+				
+				try
+				{
+					inGround = inGroundProp.getBoolean(earrow);
+					x = xprop.getInt(earrow);
+					y = yprop.getInt(earrow);
+					z = zprop.getInt(earrow);
+				}
+				catch (IllegalArgumentException e) { magicFail(e); normalWay(shooter, arrow); return; }
+				catch (IllegalAccessException e) {magicFail(e); normalWay(shooter, arrow); return;}
+				
+				Block block = null;
+				
+				if (inGround) 
+					block = arrow.getWorld().getBlockAt(x, y, z);
+				
+				EventData data = myInfo.makeData(
+						shooter,
+						block,
+						arrow,
+						arrow.getWorld());
+				
+				runRoutines(data);
+			}
+		});
+	}
+	
+	private void magicFail(Object e) {
+		tryMagic = false;
+		System.err.println("ProjectileHit magic failed: "+e.toString());
 	}
 }
