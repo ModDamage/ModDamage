7f746a84c3546198d392b99381188a42f5ba977c
Matt Peterson <ricochet1k@gmail.com>
2012-12-22 09:36:26 -0700

    Added {block}_line({linenumber})

--- src/com/ModDamage/Properties/BlockProps.java
+++ src/com/ModDamage/Properties/BlockProps.java
@@ -1,8 +1,19 @@
 package com.ModDamage.Properties;
 
-import com.ModDamage.Parsing.Property.Properties;
+import java.util.regex.Pattern;
 
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockState;
+import org.bukkit.block.Sign;
+
+import com.ModDamage.Backend.BailException;
+import com.ModDamage.EventInfo.EventData;
+import com.ModDamage.Parsing.DataProvider;
+import com.ModDamage.Parsing.FunctionParser;
+import com.ModDamage.Parsing.IDataProvider;
+import com.ModDamage.Parsing.ISettableDataProvider;
+import com.ModDamage.Parsing.SettableDataProvider;
+import com.ModDamage.Parsing.Property.Properties;
 
 public class BlockProps
 {
@@ -12,9 +23,48 @@ public class BlockProps
 		Properties.register("light", Block.class, "getLightLevel");
 		Properties.register("blocklight", Block.class, "getLightFromBlocks");
 		Properties.register("skylight", Block.class, "getLightFromSky");
+        Properties.register("type",	Block.class, "getType");
 		Properties.register("typeid", Block.class, "getTypeId", "setTypeId");
 		Properties.register("data", Block.class, "getData", "setData");
+		
+		DataProvider.registerTransformer(Block.class, "getState");
+		DataProvider.registerTransformer(Sign.class, BlockState.class);
+		
+        DataProvider.register(String.class, Sign.class, Pattern.compile("_line", Pattern.CASE_INSENSITIVE),
+                new FunctionParser<String, Sign>(Integer.class) {
+                    @SuppressWarnings("rawtypes")
+					protected ISettableDataProvider<String> makeProvider(IDataProvider<Sign> startDP, final IDataProvider[] arguments) {
+                        return new SettableDataProvider<String, Sign>(Sign.class, startDP) {
+                            public String get(Sign sign, EventData data) throws BailException {
+                                Integer line = (Integer) arguments[0].get(data);
+                                if (line == null || line < 1 || line > 4) return null;
+                                
+                                return sign.getLine(line - 1);
+                            }
+                            
+                            public void set(Sign sign, EventData data, String value) throws BailException {
+                                Integer line = (Integer) arguments[0].get(data);
+                                if (line == null || line < 1 || line > 4) return;
+                                
+                                sign.setLine(line - 1, value);
+                                sign.update();
+                            }
 
-        Properties.register("type",	Block.class, "getType");
+                            public Class<? extends String> provides() {
+                                return String.class;
+                            }
+
+							@Override
+							public boolean isSettable()
+							{
+								return true;
+							}
+							
+							public String toString() {
+								return startDP + "_line(" + arguments[0] + ")";
+							}
+                        };
+                    }
+                });
 	}
 }
