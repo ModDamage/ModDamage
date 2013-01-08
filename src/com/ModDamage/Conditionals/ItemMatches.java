dcfe19f4e0f237a7fcc4b42d87d8583407b4a5f4
Matt Peterson <ricochet1k@gmail.com>
2013-01-07 23:08:13 -0700

    Update ItemMatches to use ItemHolder

--- src/com/ModDamage/Conditionals/ItemMatches.java
+++ src/com/ModDamage/Conditionals/ItemMatches.java
@@ -4,39 +4,38 @@ import java.util.Collection;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.bukkit.inventory.ItemStack;
-
 import com.ModDamage.ModDamage;
-import com.ModDamage.Utils;
-import com.ModDamage.Parsing.DataProvider;
-import com.ModDamage.Parsing.IDataParser;
-import com.ModDamage.Parsing.IDataProvider;
 import com.ModDamage.PluginConfiguration.OutputPreset;
 import com.ModDamage.StringMatcher;
+import com.ModDamage.Utils;
 import com.ModDamage.Alias.ItemAliaser;
 import com.ModDamage.Backend.BailException;
+import com.ModDamage.Backend.ItemHolder;
 import com.ModDamage.Backend.ModDamageItemStack;
 import com.ModDamage.EventInfo.EventData;
 import com.ModDamage.EventInfo.EventInfo;
+import com.ModDamage.Parsing.DataProvider;
+import com.ModDamage.Parsing.IDataParser;
+import com.ModDamage.Parsing.IDataProvider;
 
-public class ItemMatches extends Conditional<ItemStack> 
+public class ItemMatches extends Conditional<ItemHolder> 
 {
 	public static final Pattern pattern = Pattern.compile("\\.(material|matches)\\.([\\w,]+)", Pattern.CASE_INSENSITIVE);
 	
 	private final Collection<ModDamageItemStack> items;
 	
-	public ItemMatches(IDataProvider<ItemStack> itemDP, Collection<ModDamageItemStack> items)
+	public ItemMatches(IDataProvider<ItemHolder> itemDP, Collection<ModDamageItemStack> items)
 	{
-		super(ItemStack.class, itemDP);
+		super(ItemHolder.class, itemDP);
 		this.items = items;
 	}
 	@Override
-	public Boolean get(ItemStack item, EventData data) throws BailException
+	public Boolean get(ItemHolder item, EventData data) throws BailException
 	{
 		for (ModDamageItemStack mdis : items)
 		{
 			mdis.update(data);
-			if (mdis.matches(item))
+			if (mdis.matches(item.getItem()))
 				return true;
 		}
 		return false;
@@ -51,10 +50,10 @@ public class ItemMatches extends Conditional<ItemStack>
 	
 	public static void register()
 	{
-		DataProvider.register(Boolean.class, ItemStack.class, pattern, new IDataParser<Boolean, ItemStack>()
+		DataProvider.register(Boolean.class, ItemHolder.class, pattern, new IDataParser<Boolean, ItemHolder>()
 			{
 				@Override
-				public IDataProvider<Boolean> parse(EventInfo info, IDataProvider<ItemStack> itemDP, Matcher m, StringMatcher sm)
+				public IDataProvider<Boolean> parse(EventInfo info, IDataProvider<ItemHolder> itemDP, Matcher m, StringMatcher sm)
 				{
 					Collection<ModDamageItemStack> matchedItems = ItemAliaser.match(m.group(2), info);
 					if(matchedItems == null || matchedItems.isEmpty()) return null;
