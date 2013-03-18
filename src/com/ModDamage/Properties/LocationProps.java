27818bd8bbee16a67e700896bd331fc7d0bbb324
Matt Peterson <ricochet1k@gmail.com>
2013-03-18 14:20:23 -0600

    Double location_x,y,z

--- src/com/ModDamage/Properties/LocationProps.java
+++ src/com/ModDamage/Properties/LocationProps.java
@@ -8,9 +8,9 @@ public class LocationProps
 {
 	public static void register()
 	{
-        Properties.register("x", Location.class, "getBlockX");
-        Properties.register("y", Location.class, "getBlockY");
-        Properties.register("z", Location.class, "getBlockZ");
+        Properties.register("x", Location.class, "getX");
+        Properties.register("y", Location.class, "getY");
+        Properties.register("z", Location.class, "getZ");
         Properties.register("yaw", Location.class, "getYaw");
         Properties.register("pitch", Location.class, "getPitch");
 
