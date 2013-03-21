2a9494d8186569d64cf8e2b6228a4b3ac6341bdb
Matt Peterson <ricochet1k@gmail.com>
2013-03-20 18:16:00 -0600

    Up-cast before transformers, down-cast after transformers

--- src/com/ModDamage/Parsing/DataProvider.java
+++ src/com/ModDamage/Parsing/DataProvider.java
@@ -330,6 +330,10 @@ public abstract class DataProvider<T, S> implements IDataProvider<T>
 		
 		if (want.equals(dpProvides)) return (IDataProvider<T>) dp;
 		
+
+		if (want.isAssignableFrom(dpProvides))
+			return new CastDataProvider<T>(dp, want);
+		
 		// dp doesn't match the required cls, look for any transformers that may convert it to the correct class
 		for (Entry<Class<?>, ArrayList<TransformerData<?, ?>>> entry : transformersByStart.entrySet())
 		{
@@ -348,7 +352,7 @@ public abstract class DataProvider<T, S> implements IDataProvider<T>
 			}
 		}
 		
-		if (want.isAssignableFrom(dpProvides) || dpProvides.isAssignableFrom(want))
+		if (dpProvides.isAssignableFrom(want))
 			return new CastDataProvider<T>(dp, want);
 		
 		return null;
