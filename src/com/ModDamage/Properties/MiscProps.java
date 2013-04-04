b8354599eba5e3f77f0381249b306a9638ec07ef
Matt Peterson <ricochet1k@gmail.com>
2013-04-04 15:10:11 -0600

    Added _class property for any object, and _name,_simplename for Classes

--- src/com/ModDamage/Properties/MiscProps.java
+++ src/com/ModDamage/Properties/MiscProps.java
@@ -6,5 +6,9 @@ public class MiscProps
 {
 	public static void register() {
 		Properties.register("length", String.class, "length");
+		
+		Properties.register("class", Object.class, "getClass");
+		Properties.register("name", Class.class, "getName");
+		Properties.register("simplename", Class.class, "getSimpleName");
 	}
 }
