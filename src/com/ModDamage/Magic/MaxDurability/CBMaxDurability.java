a98e85b524a59b90765db310082d96afc3dc67db
Christopher Krier <ckrier.3000@gmail.com>
2013-06-12 15:06:16 -0400

    Removed asm dependency.

--- src/com/ModDamage/Magic/MaxDurability/CBMaxDurability.java
+++ src/com/ModDamage/Magic/MaxDurability/CBMaxDurability.java
@@ -1,43 +1,41 @@
 package com.ModDamage.Magic.MaxDurability;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 import org.bukkit.inventory.ItemStack;
 
 import com.ModDamage.Magic.MagicStuff;
-import com.esotericsoftware.reflectasm.MethodAccess;
 
 public class CBMaxDurability implements IMagicMaxDurability
 {
 	final Field CraftItemStack_handle;
 
-	final MethodAccess NMSItemStack_m;
-	final int NMSItemStack_getItem;
+	final Method NMSItemStack_getItem;
 
-	final MethodAccess NMSItem_m;
-	final int NMSItem_getMaxDurability;
+	final Method NMSItem_getMaxDurability;
 	
 	public CBMaxDurability()
 	{
+		
 		Class<?> CraftItemStack = MagicStuff.safeClassForName(MagicStuff.obc + ".inventory.CraftItemStack");
 		CraftItemStack_handle = MagicStuff.safeGetField(CraftItemStack, "handle");
 		
-		NMSItemStack_m = MethodAccess.get(MagicStuff.safeClassForName(MagicStuff.nms + ".ItemStack"));
-		NMSItemStack_getItem = NMSItemStack_m.getIndex("getItem");
-
-		NMSItem_m = MethodAccess.get(MagicStuff.safeClassForName(MagicStuff.nms + ".Item"));
-		NMSItem_getMaxDurability = NMSItem_m.getIndex("getMaxDurability");
+		NMSItemStack_getItem = MagicStuff.safeGetMethod(MagicStuff.safeClassForName(MagicStuff.nms + ".ItemStack"), "getItem");
+		
+		NMSItem_getMaxDurability = MagicStuff.safeGetMethod(MagicStuff.safeClassForName(MagicStuff.nms + ".Item"), "getMaxDurability");
+		
 	}
 	
 	public int getMaxDurability(ItemStack itemStack)
 	{
 		Object handle = MagicStuff.safeGet(itemStack, CraftItemStack_handle);
 		if (handle == null) return 0;
-
-		Object item = NMSItemStack_m.invoke(handle, NMSItemStack_getItem);
+		
+		Object item = MagicStuff.safeInvoke(handle, NMSItemStack_getItem);
 		if (item == null) return 0;
 
-		Object durability = NMSItem_m.invoke(item, NMSItem_getMaxDurability);
+		Object durability = MagicStuff.safeInvoke(item, NMSItem_getMaxDurability);
 		if (durability == null || !(durability instanceof Integer)) return 0;
 
 		return (Integer) durability;
